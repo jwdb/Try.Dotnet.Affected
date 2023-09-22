d5d0dc6111666ceb17c97dc9d8b80c7563506192
Jan-Willem de Bruyn <jan.willem.de.bruyn@avivasolutions.nl>
2023-09-22 10:33:02 +0200

    Use nuke secrets for azure login

--- build/Components/Azure.Targets.cs
+++ build/Components/Azure.Targets.cs
@@ -2,11 +2,16 @@
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.Chocolatey;
 
+// ReSharper disable InconsistentNaming
 namespace Components;
 
 interface IAzureTargets : INukeBuild
 {
     [PathVariable("az")] static Tool Az;
+
+    [Parameter][Secret] string ARM_CLIENT_ID => TryGetValue(() => ARM_CLIENT_ID);
+    [Parameter][Secret] string ARM_CLIENT_SECRET => TryGetValue(() => ARM_CLIENT_SECRET);
+    [Parameter][Secret] string ARM_TENANT_ID => TryGetValue(() => ARM_TENANT_ID);
 
     Target InstallAzureCli => definition => definition
         .Executes(() => ChocolateyTasks.Chocolatey("upgrade azure-cli -y"));
@@ -15,6 +20,12 @@ interface IAzureTargets : INukeBuild
         .DependsOn(InstallAzureCli)
         .Executes(() =>
         {
-            Az("login --service-principal -u $env:ARM_CLIENT_ID -p $env:ARM_CLIENT_SECRET --tenant $env:ARM_TENANT_ID");
+            Az(new Arguments()
+                .Add("login")
+                .Add("--service-principal")
+                .Add("-u {value}", ARM_CLIENT_ID, secret: true)
+                .Add("-p {value}", ARM_CLIENT_SECRET, secret: true)
+                .Add("--tenant {value}", ARM_TENANT_ID, secret: true)
+                .RenderForExecution());
         });
 }
